ef6d0fdcd665532637dd5d16b73f80a7f8108fb8
“huangdaohong” <2224397297@qq.com>
2022-09-07 20:16:32 +0800

    日志4

--- src/app/index.js
+++ src/app/index.js
@@ -25,22 +25,22 @@ app.use(
 );
 
 app.keys = ['some secret hurr']; //cookie的签名
-app.use(
-  Koa_Session(
-    {
-      key: 'koa:sess', //cookie key (default is koa:sess)
-      maxAge: 1000 * 600 * 600, // cookie的过期时间 maxAge in ms 3分钟
-      overwrite: true, //是否可以overwrite    (默认default true)
-      httpOnly: true, //cookie是否只有服务器端可以访问 httpOnly or not (default true)
-      signed: true, //签名默认true
-      rolling: false, //在每次请求时强行设置cookie，这将重置cookie过期时间（默认：false）
-      renew: false, //(boolean) renew session when session is nearly expired,
-      // secure: true,
-      // SameSite: 'None',
-    },
-    app
-  )
-);
+// app.use(
+//   Koa_Session(
+//     {
+//       key: 'koa:sess', //cookie key (default is koa:sess)
+//       maxAge: 1000 * 600 * 600, // cookie的过期时间 maxAge in ms 3分钟
+//       overwrite: true, //是否可以overwrite    (默认default true)
+//       httpOnly: true, //cookie是否只有服务器端可以访问 httpOnly or not (default true)
+//       signed: true, //签名默认true
+//       rolling: false, //在每次请求时强行设置cookie，这将重置cookie过期时间（默认：false）
+//       renew: false, //(boolean) renew session when session is nearly expired,
+//       // secure: true,
+//       // SameSite: 'None',
+//     },
+//     app
+//   )
+// );
 
 // app.use(KoaBody());
 app.use(
