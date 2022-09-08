f5c5042559dfc9f624a73e68080d58948b0aa7ec
“huangdaohong” <2224397297@qq.com>
2022-09-08 17:15:58 +0800

    qq24

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -316,11 +316,12 @@ class UserController {
         // 存在
         res2.token = jwt.sign(res2, JWT_SECRET, { expiresIn: '7d' });
 
-        ctx.body = {
-          code: 0,
-          message: '登录成功',
-          data: res2,
-        };
+        // ctx.body = {
+        //   code: 0,
+        //   message: '登录成功',
+        //   data: res2,
+        // };
+        ctx.redirect(`https://hdhblog.cn/home`);
       } else {
         let { password, ...res1 } = await createUser(obj);
         /** 从这里到封装 都是改变我获取的用户信息存储到数据库里面，根据数据库的存储，创建新用户，如果有
