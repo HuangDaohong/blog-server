5f0eabb53f5e5e226e5bdd9b1c869ee44695357f
“huangdaohong” <2224397297@qq.com>
2022-09-08 11:39:30 +0800

    qq2

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const Router = require('koa-router');
-const { isDev } = require('../config/config.default');
 
 /* 生产环境下使用，因为nginx代理了api */
-// const router = new Router();
+const router = new Router();
 
 /* 开发环境下使用 ,配置了.gitignore*/
-const router = new Router({ prefix: isDev ? '/api' : '' });
+// const router = new Router({ prefix: '/api' });
 
 fs.readdirSync(__dirname).forEach((file) => {
   if (file !== 'index.js') {
