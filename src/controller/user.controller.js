97ea8aba9b5d70700a8f2660bdeb6acc98ab39b0
“huangdaohong” <2224397297@qq.com>
2022-09-08 19:01:58 +0800

    qq27

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -334,7 +334,8 @@ class UserController {
         //   message: '登录成功',
         //   data: res2,
         // };
-        ctx.redirect(`https://hdhblog.cn/home?qqname=${res2.name}`);
+        // 携带token跳转到首页
+        ctx.redirect(`https://hdhblog.cn/about?qqname=${res2.name}`);
       } else {
         let { password, ...res1 } = await createUser(obj);
         /** 从这里到封装 都是改变我获取的用户信息存储到数据库里面，根据数据库的存储，创建新用户，如果有
